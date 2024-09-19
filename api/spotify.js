cf425e4921522f97b4f368b27ee459174699bbba
GwFirman <firmanzamzamiaziz@gmail.com>
2024-09-19 22:51:58 +0700

    fix : api

--- api/spotify.js
+++ api/spotify.js
@@ -28,14 +28,10 @@ module.exports = async (req, res) => {
     }
   });
 
-  const currentPlayingResponse = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-    headers: {
-      'Authorization': `Bearer ${access_token}`
-    }
-  });
+  
 
   const topTracksData = await topTracksResponse.json();
-  const currentPlayingData = await currentPlayingResponse.json();
+  // const currentPlayingData = await currentPlayingResponse.json();
 
   // Mengirim data top tracks ke frontend
   
